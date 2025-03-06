33739e77be4d822a51c16915c9ce53de4d948943
Pedro Dias <pedrocorchogdev@gmail.com>
2025-03-06 17:04:00 -0300

    feat: add user service

---
+++ service-user/src/main/java/dev/pedrodias/service_user/config/exception/HashingException.java
@@ -0,0 +1,7 @@
+package dev.pedrodias.service_user.config.exception;
+
+public class HashingException extends RuntimeException {
+    public HashingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ service-user/src/main/java/dev/pedrodias/service_user/config/feign/FeignConfig.java
@@ -0,0 +1,9 @@
+package dev.pedrodias.service_user.config.feign;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients(basePackages = "dev.pedrodias.serviceauth.core.repositories")
+public class FeignConfig {
+}
